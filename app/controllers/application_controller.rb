9b59010e60f7d7ad1c9b70325fa672e4b10050a0
Lucas <lucastelesleiro7@gmail.com>
2026-06-16 13:06:10 -0300

    após logar ir para root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:accept_invitation, keys: %i[nome matricula perfil])
   end
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   def after_sign_out_path_for(resource)
     new_user_session_path
   end
