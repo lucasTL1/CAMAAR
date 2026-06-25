31ef5d76c531f613c8b531eb87b51e24ae7598ef
Lucas <lucastelesleiro7@gmail.com>
2026-06-25 17:25:21 -0300

    Protege turmas_controller e adiciona happy/sad path nos testes de request

--- app/controllers/turmas_controller.rb
+++ app/controllers/turmas_controller.rb
@@ -1,4 +1,5 @@
 class TurmasController < ApplicationController
+  before_action :authenticate_user!
   def index
     if current_user&.departamento.present?
       @turmas = Turma.where(departamento: current_user.departamento)
--- spec/requests/turmas_spec.rb
+++ spec/requests/turmas_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Turmas", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/turmas"
-      expect(response).not_to have_http_status(:not_found)
+  include Devise::Test::IntegrationHelpers
+
+  describe "GET /turmas" do
+    
+    context "quando o usuário está logado (Happy Path)" do
+      let(:user) { User.create!(email: "aluno@unb.br", password: "password123", nome: "Aluno", matricula: "123456", perfil: "discente") }
+
+      it "acessa a página de turmas com sucesso" do
+        sign_in user
+        get "/turmas"
+        expect(response).to have_http_status(:success)
+      end
     end
+
+    context "quando o usuário não está logado (Sad Path)" do
+      it "bloqueia o acesso e redireciona para a tela de login" do
+        # Fazemos a requisição direto, sem usar o sign_in antes
+        get "/turmas"
+        
+        # O sistema deve barrar e jogar para o login (status 302 Redirect)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
   end
 end
